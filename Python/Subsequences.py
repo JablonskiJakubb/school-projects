af20c1729d5fd76cff3728dcb5773e948bc2c473
Jacob Jablonski <114925147+JablonskiJakubb@users.noreply.github.com>
2025-01-08 22:37:24 +0100

    Update Subsequences.py

--- Python/Subsequences.py
+++ Python/Subsequences.py
@@ -10,7 +10,7 @@ def get_subsequence(string):
     """
     subsequence = ""
     for i in range(len(string)):
-        if i % 2 == 0:  # Select characters at even indices
+        if i % 2 == 0:  # Select characters at even indexes
             subsequence += string[i]
     return subsequence
 
@@ -23,4 +23,25 @@ print(f"Subsequence: {result}")
 #------------------------------------------------------------------------------------------------------------
 # exercises
 #------------------------------------------------------------------------------------------------------------
-# Exercise: Longest Increasing Subsequence (LIS)
+# Exercise: Longest Increasing Continuous Subsequence (LICS)
+
+def LICS(num):
+    max = 0
+    result = 0
+    for i in range(len(num)):
+        for j in range(i,len(num)):
+            if num[i] <= num[j]:
+                max += 1
+            if num[j-1] > num[j]:
+                max -= 1
+        if result < max:
+            result = max
+        max = 0
+    return result
+
+# Example usage
+num = [10, 9, 2, 5, 3, 7, 101, 18]
+print(LICS(num))
+# Output: 3
+
+
